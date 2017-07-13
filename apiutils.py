118d40ff94584e21bf202dac554c9bc8e51121a4
Mike Caesar <mcaesar@dxc.com>
2017-07-13 22:15:43 +1000

    First Commit

---
+++ apiutils.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+import json
+import requests
+import getpass
+import os
+import sys
+from config import *
+
+# Get a token. The argument defines the expiry time in seconds. Defaults to 20.
+def get_token(exp=20):
+    # Prompt for the password if it isn't set in config
+    # Use getpass so that password is not displayed as it is typed
+    global adminpass
+    try:
+        adminpass
+    except NameError:
+        adminpass = getpass.getpass('Enter the password for the qipman admin user : ')
+    # Get a token  
+    headers = {'Content-Type': 'application/json'}
+    data = "{\"username\" : \"qipman\",\"password\": \"" + adminpass + "\",\"expires\": " + str(exp) + "}"
+    
+    # loginurl comes from config imported above
+    r = requests.post(loginurl, headers=headers, verify=False, data=data)
+    return r.headers.get('authentication')
+
+def chkstatus(code,content):
+    if (code == 200 or code == 201):
+        print('Success')
+    else:
+        print('Failed:')
+	print(content)
+        sys.exit(1)
---
+++ config.py
@@ -0,0 +1,9 @@
+esmhost = 'vqip'
+esmport = '8080'
+org = 'VitalQIP%20Organization'
+domain = 'pyrdev.dxc.com'
+adminpass = 'qipman'
+
+# Do not change items below here
+apiurl = 'http://' + esmhost + ':' +esmport + '/api/v1/' + org 
+loginurl = 'http://' + esmhost + ':' +esmport + '/api/login'
